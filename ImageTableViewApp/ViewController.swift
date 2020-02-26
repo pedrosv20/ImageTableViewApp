74e9ec79f52378ae06d3b5ea251266c6f00bd03f
Pedro <pedro1silva1vargas@gmail.com>
2020-02-26 14:47:02 -0300

    Initial Commit

---
+++ ImageTableViewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageTableViewApp
+//
+//  Created by Pedro Vargas on 26/02/20.
+//  Copyright © 2020 Pedro Vargas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
